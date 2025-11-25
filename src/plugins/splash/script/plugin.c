997f11aea946740882e70230981aa96059ce04d6
dongshengyuan <dongshengyuan@uniontech.com>
2025-11-25 15:58:21 +0800

    add fix log

--- src/plugins/splash/script/plugin.c
+++ src/plugins/splash/script/plugin.c
@@ -257,18 +257,7 @@ start_script_animation (ply_boot_splash_plugin_t *plugin)
         plugin->script_state = script_state_new (plugin);
 
 
-    // === 打印 uos-ssd-logo.script 读取到的分辨率 ===
-    for (node = ply_list_get_first_node (plugin->displays);
-         node != NULL;
-         node = ply_list_get_next_node (plugin->displays, node)) {
-        ply_pixel_display_t *display = ply_list_node_get_data (node);
-        unsigned int width, height;
-        ply_pixel_display_get_size(display, &width, &height);
-        ply_debug("uos-ssd-logo.script resolution: width=%u height=%u", width, height);
-        // 如果 ply_debug 编译不过，可以用下面这一行替换：
-        // fprintf(stderr, "uos-ssd-logo.script resolution: width=%u height=%u\n", width, height);
-    }
-    // === 打印结束 ===
+
 
         for (node = ply_list_get_first_node (plugin->script_env_vars);
              node != NULL;
